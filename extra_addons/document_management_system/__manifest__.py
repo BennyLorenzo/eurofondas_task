df3b6838a88e0bfde0ebc78c837e62d8ce905713
BennyLorenzo <bennylorenzo13@gmail.com>
2024-07-18 17:29:58 +0300

    Document Management System task v.1.0.0

---
+++ extra_addons/document_management_system/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name':'Document Management System',
+    'author':'Benny Lorenzo',
+    'website':'www.github.com/BennyLorenzo',
+    'summary':'A simple user interface for managing documents',
+    'category':'Documents',
+    'data': [
+            'security/ir.model.access.csv',
+            'views/menu.xml',
+            'views/document.xml',
+            'views/employee.xml'
+    ]
+}
